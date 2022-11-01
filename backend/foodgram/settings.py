cdc1818d929b770407ef5c6e42ab71857a232b67
Denis <deni_95@list.ru>
2022-11-01 15:30:44 +0500

    12

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT')
     }
 }
